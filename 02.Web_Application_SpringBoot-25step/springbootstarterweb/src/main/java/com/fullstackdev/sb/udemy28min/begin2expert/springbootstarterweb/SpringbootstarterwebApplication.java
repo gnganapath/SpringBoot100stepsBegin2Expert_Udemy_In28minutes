ed6bea4718287c2b470f55c8c3153b9b308f8bf3
gnganpath <esourcej@gmail.com>
2020-06-23 13:40:07 +0530

    @controller + ResponseBody = @RestController step10:02-part2

---
+++ 02.Web_Application_SpringBoot-25step/springbootstarterweb/src/main/java/com/fullstackdev/sb/udemy28min/begin2expert/springbootstarterweb/SpringbootstarterwebApplication.java
@@ -0,0 +1,13 @@
+package com.fullstackdev.sb.udemy28min.begin2expert.springbootstarterweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstarterwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstarterwebApplication.class, args);
+	}
+
+}
---
+++ 02.Web_Application_SpringBoot-25step/springbootstarterweb/src/test/java/com/fullstackdev/sb/udemy28min/begin2expert/springbootstarterweb/SpringbootstarterwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackdev.sb.udemy28min.begin2expert.springbootstarterweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootstarterwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
